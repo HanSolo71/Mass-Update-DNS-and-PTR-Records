de6880dc2cbb03d6c0c73355e367736216fc3560
HanSolo71 <HanSolo552@gmail.com>
2018-10-29 12:58:11 -0400

    Add files via upload

---
+++ Update DNS DR.ps1
@@ -0,0 +1,17 @@
+﻿#Change the file included to have the correct zone in it
+#Change the file included to have correct PTR zone in it
+$DNSservers= @(“Domain Controller”)
+## Filepath to script and CSV files
+$FP = “\\fileshare\Update DNS”
+CD $FP
+Foreach($DNSserver in $DNSservers)
+{
+ Import-CSV .\TestADelete.csv| foreach {
+    dnscmd $DNSserver /RecordDelete $_.zone $_.hostname A $_.ip /f}
+  Import-CSV .\TestACreate.csv | foreach {
+    dnscmd $DNSserver /RecordAdd $_.zone $_.hostname A $_.ip}
+  Import-CSV .\DNSPTRDelete.csv | foreach {
+    dnscmd $DNSserver /RecordDelete $_.reversezone $_.lowip PTR $_.fqdn /f}
+  Import-CSV .\DNSPTRCreate.csv | foreach {
+    dnscmd $DNSserver /RecordAdd $_.reversezone $_.lowip PTR $_.fqdn}
+}
